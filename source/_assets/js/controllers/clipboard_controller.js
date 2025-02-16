903842c64226934aca553574d5854434d2e1835e
Tony Messias <tonysm@hey.com>
2025-02-15 23:00:09 -0300

    Display a check mark when code is copied

--- source/_assets/js/controllers/clipboard_controller.js
+++ source/_assets/js/controllers/clipboard_controller.js
@@ -1,8 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  #timeout;
+
   copy() {
+    if (this.#timeout) {
+      clearTimeout(this.#timeout);
+    }
+
     navigator.clipboard.writeText(this.#code);
+    this.element.setAttribute("data-copied", true);
+
+    this.#timeout = setTimeout(
+      () => this.element.removeAttribute("data-copied"),
+      3000,
+    );
   }
 
   get #code() {
