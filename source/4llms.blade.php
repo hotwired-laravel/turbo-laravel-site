ced5a401e6683d744ffa27cc8a2ce2b4f6eb3d69
Tony Messias <tonysm@hey.com>
2025-05-03 22:57:09 -0300

    Adds the llms path

--- source/4llms.blade.php
+++
@@ -1,7 +0,0 @@
-<docs>
-@foreach ($docs as $doc)
-    <page title="{{ $doc->title }}" url="{{ $doc->getUrl() }}">
-        {{ html_entity_decode(strip_tags($doc->getContent()), ENT_QUOTES | ENT_HTML5) }}
-    </page>
-@endforeach
-</docs>
---
+++ source/llms.blade.php
@@ -0,0 +1,9 @@
+# Turbo Laravel
+
+Turbo Laravel is a community maintenaned package that integrates Laravel with Hotwire.
+
+## Table of Contents
+
+@foreach ($docs as $doc)
+- [{{ $doc->title }}]({{ $doc->getUrl() }})
+@endforeach
